cd4f779399c0b79ddc6f34c8248f3376f6eb6e72
jbacon <julien.baccon.1@gmail.com>
2023-04-29 06:37:00 -0400

    testing coverage

--- entrypoint.sh
+++ entrypoint.sh
@@ -154,7 +154,7 @@ fi
 if [[ $BUILD_MODE =~ ^($TEST_MODE)$ ]]
 then
   greenPrint "Running the firmware tests..."
-  zephyr/scripts/twister --coverage -T app/
+  zephyr/scripts/twister --coverage -p qemu_cortex_m3 -T app/
   testResut=$?
   greenPrint "Moving test artefacts..."
   moveTestArtefacts || exitError "ERROR: Unable to move test artefacts."
