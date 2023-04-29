16196eac3d0d2fcea684d7d6cdb429cdaaeb47fd
jbacon <julien.baccon.1@gmail.com>
2023-04-29 06:04:28 -0400

    testing adding coverage with test mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -154,7 +154,7 @@ fi
 if [[ $BUILD_MODE =~ ^($TEST_MODE)$ ]]
 then
   greenPrint "Running the firmware tests..."
-  zephyr/scripts/twister -T app/
+  zephyr/scripts/twister --coverage -T app/
   testResut=$?
   greenPrint "Moving test artefacts..."
   moveTestArtefacts || exitError "ERROR: Unable to move test artefacts."
