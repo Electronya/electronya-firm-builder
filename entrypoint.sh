087ad26f00940a9d31d1952a656ab7f8480efe63
jbacon <julien.baccon.1@gmail.com>
2023-04-30 07:06:57 -0400

    changing target path for test

--- entrypoint.sh
+++ entrypoint.sh
@@ -154,7 +154,7 @@ fi
 if [[ $BUILD_MODE =~ ^($TEST_MODE)$ ]]
 then
   greenPrint "Running the firmware tests..."
-  zephyr/scripts/twister -T app/
+  zephyr/scripts/twister -T app/tests/unit/
   testResut=$?
   greenPrint "Moving test artefacts..."
   moveTestArtefacts || exitError "ERROR: Unable to move test artefacts."
